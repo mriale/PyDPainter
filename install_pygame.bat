6f68fcdcfdcd373d5e8944a53c4df72607ce3a16
Mark Riale <mriale@gmail.com>
2023-03-19 23:12:46 -0400

    Fix Windows installer to upgrade pygame and numpy

--- install_pygame.bat
+++ install_pygame.bat
@@ -2,8 +2,8 @@
 get_python_path.py >%TMP%\python_path.txt
 <%TMP%\python_path.txt set /p python_path=
 echo Installing pygame package...
-%python_path% -m pip install pygame --pre
+%python_path% -m pip install pygame -U
 echo Installing numpy package...
-%python_path% -m pip install numpy
+%python_path% -m pip install numpy -U
 echo Done.
 pause
