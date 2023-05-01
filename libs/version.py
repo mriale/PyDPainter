a6c514330caa479ba65df0bd36a027588005ac80
Mark Riale <mriale@gmail.com>
2023-04-30 22:38:55 -0400

    Bump version to 1.0.9

--- libs/version.py
+++ libs/version.py
@@ -8,5 +8,5 @@ config = None
 def version_set_config(config_in):
     global config
     config = config_in
-    config.version = "1.0.9 dev"
+    config.version = "1.0.9"
     config.copyright = "\xA92023 Mark Riale"
