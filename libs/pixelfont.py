9890f20fda06fde387e03fa1aff7882443b2228c
Caleb Riale <calebriale@gmail.com>
2025-10-08 14:44:36 -0400

    Fix font rendering, change pixelfont.py standalone behavior
    Changed font image height and redid main() (currently non-functional due to import issue)

--- libs/pixelfont.py
+++ libs/pixelfont.py
@@ -3,6 +3,7 @@
 
 # PixelFont bitmap font renderer
 
+import sys
 import re
 import numpy as np
 import contextlib
@@ -26,7 +27,7 @@ class PixelFont(object):
         self.image = gadget.imgload(name, scaledown=scaledown, scaleX=scaleX, scaleY=scaleY)
         xs,ys = self.image.get_size()
         self.xsize = xs // 32
-        self.ysize = ys // 8
+        self.ysize = ys // 40  #should probably be calculated differently
         self.image_color = {}
 
     def calcwidth(self, string):
@@ -51,6 +52,7 @@ class PixelFont(object):
             image_blit = self.image
 
         for c in string:
+            if ord(c) > 2560 and __debug__: c = "\x7f"
             yo = (ord(c) // 32) * self.ysize
             xo = (ord(c) %  32) * self.xsize
 
@@ -66,12 +68,18 @@ def char_range(c1, c2):
     for c in range(ord(c1), ord(c2)+1):
         yield chr(c)
 
+ypos = 0
+def printrow(screen, font, ypos, string, fgcolor=None, bgcolor=None):
+    font.blitstring(screen, (0,ypos), string, fgcolor=fgcolor, bgcolor=bgcolor)
+    ypos += font.ysize
+    return ypos
+
 def main():
 
     #Initialize the configuration settings
     pygame.init()
         
-    sx,sy = 800,600
+    sx,sy = 1280,960
     
     screen = pygame.display.set_mode((sx,sy))
 
@@ -82,28 +90,27 @@ def main():
     f4 = PixelFont("jewel32.png", 32)
     running = 1
 
+    yscroll = 0
+
+    if len(sys.argv) == 2:
+        with open(sys.argv[1],"r") as textfile:
+            displaytext = textfile.read().split("\n") #open text file from command line argument
+    else:
+        displaytext = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijkmlnopqrstuvwxyz"]
+        displaytext += list([''.join(map(chr,range(i*32,(i+1)*32))) for i in range(40)])
+
     while running:
-        
         screen.fill((0,0,0))
-        f.blitstring(screen, (0,0), "Jewel 8", fgcolor=(255,255,0), bgcolor=(255,0,0))
-        f.blitstring(screen, (0,8), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", fgcolor=(255,0,0))
-        f.blitstring(screen, (0,16), "abcdefghijkmlnopqrstuvwxyz", fgcolor=(0,0,255))
-        for i in range(0,8):
-            f.blitstring(screen, (0,24+(i*8)), char_range(chr(i*32),chr(((i+1)*32)-1)))
+        ypos = -yscroll*8
+        yscroll += pygame.key.get_pressed()[K_DOWN] - pygame.key.get_pressed()[K_UP] #arrow keys to scroll
 
-        f2.blitstring(screen, (0,100), "Jewel 16", fgcolor=(255,255,0), bgcolor=(255,0,0))
-        f2.blitstring(screen, (0,116), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", fgcolor=(255,0,0))
-        f2.blitstring(screen, (0,132), "abcdefghijkmlnopqrstuvwxyz", fgcolor=(0,0,255))
-        for i in range(0,8):
-            f2.blitstring(screen, (0,148+(i*16)), char_range(chr(i*32),chr(((i+1)*32)-1)))
+        for font in [f, f2, f4]:
+            ypos=printrow(screen, font, ypos, "Jewel "+str(font.ysize), fgcolor=(255,255,0), bgcolor=(255,0,0))
+            for textrow in displaytext:
+                ypos=printrow(screen, font, ypos, textrow)
 
-        f4.blitstring(screen, (0,300), "Jewel 32", fgcolor=(255,255,0), bgcolor=(255,0,0))
-        f4.blitstring(screen, (0,332), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", fgcolor=(0,0,255))
-        f4.blitstring(screen, (0,364), "abcdefghijkmlnopqrstuvwxyz", fgcolor=(0,0,255))
-        for i in range(0,8):
-            f4.blitstring(screen, (0,396+(i*32)), char_range(chr(i*32),chr(((i+1)*32)-1)))
-
-        for event in config.xevent.get():
+        #for event in config.xevent.get():
+        for event in pygame.event.get():
             if event.type == QUIT:
                 running = 0
             elif event.type == KEYDOWN:
