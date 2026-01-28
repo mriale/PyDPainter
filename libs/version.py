a82398882ddd9fe378152c920bff9588c546d482
Mark Riale <mriale@gmail.com>
2026-01-27 19:39:51 -0500

    Bump version to 2.3.0

--- libs/version.py
+++ libs/version.py
@@ -8,5 +8,5 @@ config = None
 def version_set_config(config_in):
     global config
     config = config_in
-    config.version = "2.2.1"
+    config.version = "2.3.0 beta"
     config.copyright = "\xA92026 Mark Riale"
